2a7d82bdc33d8cb17dc52c25399383f6de5c24a6
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-12 00:54:43 +0300

    update displayed bot data

--- src/components/YourBotArmy.jsx
+++ src/components/YourBotArmy.jsx
@@ -3,29 +3,31 @@ import "./BotCollection.css";
 
 function YourBotArmy({ enlistedBots, onRemove }) {
   return (
-    <div className="main">
-      {enlistedBots.map((bot) => (
-        <div key={bot.id} className="container">
-          <div className="wrapper">
-            <div className="bot-image">
-              <img src={bot.avatar_url} alt={bot.name} />
+    <div>
+        <h3>My Bot Army</h3>
+      <div className="main">
+        {enlistedBots.map((bot) => (
+          <div key={bot.id} className="container">
+            <div className="wrapper">
+              <div className="bot-image">
+                <img src={bot.avatar_url} alt={bot.name} />
+              </div>
+              <h3>Name: {bot.name}</h3>
+              <p>Health: {bot.health}</p>
+              <p>Damage: {bot.damage}</p>
+              <p>Armor: {bot.armor}</p>
+              <p>Class: {bot.bot_class}</p>
+              <p>Created at: {bot.created_at}</p>
+              <p>Updated at: {bot.updated_at}</p>
             </div>
-            <h3>Name: {bot.name}</h3>
-            <p>Health: {bot.health}</p>
-            <p>Damage: {bot.damage}</p>
-            <p>Armor: {bot.armor}</p>
-            <p>Class: {bot.bot_class}</p>
-            <p>Catch Phrase: {bot.catchphrase}</p>
-            <p>Created at: {bot.created_at}</p>
-            <p>Updated at: {bot.updated_at}</p>
+            <div class="button-wrapper">
+              <button class="btn outline" onClick={() => onRemove(bot)}>
+                Remove
+              </button>
+            </div>
           </div>
-          <div class="button-wrapper">
-            <button class="btn outline" onClick={() => onRemove(bot)}>
-              Remove
-            </button>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
