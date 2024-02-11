3be3f51a7fde6de527a5f1c3becc447083d909b9
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-11 20:24:58 +0300

    update bot information

--- src/components/YourBotArmy.jsx
+++ src/components/YourBotArmy.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import './BotCollection.css'
+import React from "react";
+import "./BotCollection.css";
 
-function YourBotArmy({enlistedBots, onRemove}) {
+function YourBotArmy({ enlistedBots, onRemove }) {
   return (
-    <div className='main'>
+    <div className="main">
       {enlistedBots.map((bot) => (
         <div key={bot.id} className="container">
-          <div className='wrapper'>
-            <div className='bot-image'>
-            <img src={bot.avatar_url} alt={bot.name} />
+          <div className="wrapper">
+            <div className="bot-image">
+              <img src={bot.avatar_url} alt={bot.name} />
             </div>
-          <h3>Name: {bot.name}</h3>
-          <p>Health: {bot.health}</p>
-          <p>Damage: {bot.damage}</p>
-          <p>Armor: {bot.armor}</p>
-          <p>Class: {bot.bot_class}</p>
-          <p>Health: {bot.health}</p>
+            <h3>Name: {bot.name}</h3>
+            <p>Health: {bot.health}</p>
+            <p>Damage: {bot.damage}</p>
+            <p>Armor: {bot.armor}</p>
+            <p>Class: {bot.bot_class}</p>
+            <p>Catch Phrase: {bot.catchphrase}</p>
+            <p>Created at: {bot.created_at}</p>
+            <p>Updated at: {bot.updated_at}</p>
           </div>
-          <div class="button-wrapper"> 
-              <button class="btn outline" onClick={()=>onRemove(bot)}>Remove</button>
+          <div class="button-wrapper">
+            <button class="btn outline" onClick={() => onRemove(bot)}>
+              Remove
+            </button>
           </div>
         </div>
       ))}
@@ -26,4 +30,4 @@ function YourBotArmy({enlistedBots, onRemove}) {
   );
 }
 
-export default YourBotArmy
+export default YourBotArmy;
