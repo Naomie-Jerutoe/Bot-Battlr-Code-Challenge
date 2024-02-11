3b762bef3be955a7339e4dc56d5240aa8a69387b
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-11 20:04:32 +0300

    update code format

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom/cjs/react-router-dom.min'
-import './NavBar.css'
+import React from "react";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import "./NavBar.css";
 
 function NavBar() {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <ul>
-        <li><NavLink to="/" style={{color:"white"}}>Bot Collection</NavLink></li>
-        <li><NavLink to="/myarmy" style={{color:"white"}}>My Army</NavLink></li>
+        <li>
+          <NavLink to="/" style={{ color: "white" }}>
+            Bot Collection
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/myarmy" style={{ color: "white" }}>
+            My Army
+          </NavLink>
+        </li>
       </ul>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
