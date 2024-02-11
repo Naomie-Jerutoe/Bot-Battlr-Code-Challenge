8abf54cc6ad31258dca8c89ac9969bdb2fd24e31
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-11 19:44:01 +0300

    feat:add sort and clicked bots functionality

--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -1,29 +1,80 @@
-import './BotCollection.css'
+import { useState } from "react";
+import "./BotCollection.css";
+import BotSpecs from "./BotSpecs";
+import SortBar from "./SortBar";
 
-function BotCollection({bots, onEnlist, onDelete}) {
+function BotCollection({ bots, onEnlist, onDelete }) {
+  const [clickedBot, setClickedBot] = useState(null);
+  const [sortBots, setSortBots] = useState("All");
+
+  const handleBotClick = (bot) => {
+    setClickedBot(bot);
+  };
+
+  const handleGoBack = () => {
+    setClickedBot(null);
+  };
+
+  const handleSortChange = (option) => {
+    setSortBots(option);
+  };
+
+  const sortedBots = () => {
+    switch (sortBots) {
+      case "health":
+        return [...bots].sort((a, b) => b.health - a.health);
+      case "damage":
+        return [...bots].sort((a, b) => a.damage - b.damage);
+      case "armor":
+        return [...bots].sort((a, b) => b.armor - a.armor);
+      default:
+        return bots;
+    }
+  };
+
   return (
-    <div className='main'>
-      {bots.map(bot=>(
-        <div key={bot.id} className="container">
-          <div className='wrapper'>
-            <div className='bot-image'>
-            <img src={bot.avatar_url} alt={bot.name} />
+    <div>
+      <div className="sort">
+        <SortBar onSortChange={handleSortChange} />
+      </div>
+      <div className="main">
+        {clickedBot ? (
+          <BotSpecs
+            bot={clickedBot}
+            onEnlist={onEnlist}
+            onGoBack={handleGoBack}
+          />
+        ) : (
+          sortedBots().map((bot) => (
+            <div key={bot.id} className="container">
+              <div
+                className="wrapper"
+                title="Click to see more"
+                onClick={() => handleBotClick(bot)}
+              >
+                <div className="bot-image">
+                  <img src={bot.avatar_url} alt={bot.name} />
+                </div>
+                <h3>Name: {bot.name}</h3>
+                <p>Health: {bot.health}</p>
+                <p>Damage: {bot.damage}</p>
+                <p>Armor: {bot.armor}</p>
+              </div>
+              <div class="button-wrapper">
+                <button
+                  class="btn fill"
+                  title="Delete Bot"
+                  onClick={() => onDelete(bot.id)}
+                >
+                  X
+                </button>
+              </div>
             </div>
-          <h3>Name: {bot.name}</h3>
-          <p>Health: {bot.health}</p>
-          <p>Damage: {bot.damage}</p>
-          <p>Armor: {bot.armor}</p>
-          <p>Class: {bot.bot_class}</p>
-          <p>Health: {bot.health}</p>
-          </div>
-          <div class="button-wrapper"> 
-              <button class="btn outline" onClick={() => onEnlist(bot)}>ENLIST</button>
-              <button class="btn fill" title='Delete Bot' onClick={()=>onDelete(bot.id)}>X</button>
-          </div>
-        </div>
-      ))}
+          ))
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default BotCollection
+export default BotCollection;
