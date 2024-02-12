572e4b56da227795879a7eb097b884711272a65b
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-13 00:14:39 +0300

    feat: add filter bar with options for bot class

--- src/components/SortBar.jsx
+++ src/components/SortBar.jsx
@@ -1,22 +1,43 @@
 import React from "react";
 import "./SortBar.css";
 
-function SortBar({ onSortChange }) {
+function SortBar({ onSortChange, onClassChange }) {
   return (
-    <div className="sort-bar">
-      <label style={{color:"white"}}>
-        Sort Bots By:
-        <select
-          name="sort"
-          id="sort"
-          onChange={(e) => onSortChange(e.target.value)}
-        >
-          <option value="all">All</option>
-          <option value="health">Health</option>
-          <option value="damage">Damage</option>
-          <option value="armor">Armor</option>
-        </select>
-      </label>
+    <div>
+      <div className="sort-bar">
+        <label style={{ color: "white" }}>
+          Sort Bots By:
+          <select
+            name="sort"
+            id="sort"
+            onChange={(e) => onSortChange(e.target.value)}
+          >
+            <option value="all">All</option>
+            <option value="health">Health</option>
+            <option value="damage">Damage</option>
+            <option value="armor">Armor</option>
+          </select>
+        </label>
+      </div>
+
+      <div className="filter-bar">
+        <label style={{ color: "white" }}>
+          Filter Bots By Class:
+          <select
+            name="filter"
+            id="filter"
+            onChange={(e) => onClassChange(e.target.value)}
+          >
+            <option value="All">All</option>
+            <option value="support">Support</option>
+            <option value="medic">Medic</option>
+            <option value="assault">Assault</option>
+            <option value="defender">Defender</option>
+            <option value="captain">Captain</option>
+            <option value="witch">Witch</option>
+          </select>
+        </label>
+      </div>
     </div>
   );
 }
