a15c7c05d990d2aceaf2922d5d85781a67dbefbc
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-13 04:03:16 +0300

    update botspecs

--- src/components/BotSpecs.jsx
+++ src/components/BotSpecs.jsx
@@ -3,25 +3,29 @@ import React from "react";
 function BotSpecs({ bot, onEnlist, onGoBack }) {
   return (
     <div key={bot.id} className="container">
-      <div className="wrapper">
-        <div className="bot-image">
-          <img src={bot.avatar_url} alt={bot.name} />
-        </div>
+      <div className="bot-image">
+        <img src={bot.avatar_url} alt={bot.name} />
+      </div>
+      <div className="bot-details">
         <h3>Name: {bot.name}</h3>
         <p>Health: {bot.health}</p>
         <p>Damage: {bot.damage}</p>
         <p>Armor: {bot.armor}</p>
+        <p style={{ fontSize: "9px" }}>Catch Phrase: {bot.catchphrase}</p>
         <p>Class: {bot.bot_class}</p>
-        <p>Catch Phrase: {bot.catchphrase}</p>
         <p>Created at: {bot.created_at}</p>
         <p>Updated at: {bot.updated_at}</p>
       </div>
-      <div class="button-wrapper">
-        <button class="btn outline" title="Enlist the bot" onClick={() => onEnlist(bot)}>
+      <div className="button-wrapper">
+        <button
+          className="btn outline"
+          title="Enlist the bot"
+          onClick={() => onEnlist(bot)}
+        >
           ENLIST
         </button>
         <button
-          class="btn outline"
+          className="btn outline"
           title="Go Back to List"
           onClick={() => onGoBack()}
         >
